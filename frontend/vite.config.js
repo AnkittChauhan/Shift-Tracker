12b6da606aac89cb022176a17dbb0b79251054a3
AnkittChauhan <ankittrajput.4@gmail.com>
2025-03-26 20:09:32 +0530

    Modified vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   optimizeDeps: {
-    include: ['axios']
+    include: ['axios'] // Pre-bundle axios
   },
   build: {
     commonjsOptions: {
-      transformMixedEsModules: true
+      transformMixedEsModules: true // Helps with module interop
     },
     rollupOptions: {
-      external: ['axios']
+      external: []
     }
   }
-})
+});
